b5ba0008aa813a793595584cdf7a88ba07b1ee0b
Roger Farrerons <rugifa@hotmail.com>
2023-07-24 00:04:23 +0200

    Layout component stuffs

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,42 +1,75 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
-
-import "./styles.css"
+import logo from "../assets/logo.png";
+import "./styles.css";
+import Connect from "../components/Buttons/Connect";
+import { Network } from "../components/Buttons/Network";
 
 const Navigation = () => (
   <section class="w-full px-8 text-gray-700 bg-white" className="menu-bg">
-    <div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl" >
+    <div class="container flex flex-col flex-wrap items-center justify-between py-3 mx-auto md:flex-row max-w-full">
       <div class="relative flex flex-col md:flex-row">
         <a
-          href="#_"
+          href="#"
           class="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0"
         >
-          <span class="mx-auto text-xl font-black leading-none text-gray-900 select-none">
-            AI<span class="text-indigo-600">.</span>
+          <img src={logo} width="20%" height="20%" />
+
+          <span class="text-xl select-none text-white ml-5">
+            FED AI<span class="text-indigo-600">.</span>
           </span>
         </a>
 
-        <nav class="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
+        {/* <a
+          class="group text-pink-500 transition-all duration-300 ease-in-out"
+          href="#"
+        >
+          <span class="bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+            This text gets 'underlined' on hover
+          </span>
+        </a> 
+        <NavLink
+            to="/"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "active" : ""
+            }
+            class={
+              "mr-5 font-medium leading-6 text-white-600 hover:text-yellow-200 group transition-all duration-300 ease-in-out"
+            }
+          >
+            <span class="bg-left-bottom bg-gradient-to-r from-yellow-500 to-yellow-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+              INICIO
+            </span>
+          </NavLink>
+        */}
+
+        <nav class="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-gray-200">
           <NavLink
             to="/"
-            className={
-              "mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900"
+            className={({ isActive }) =>
+              isActive
+                ? "mr-5 font-medium leading-6 text-[#ffff00] hover:text-[#ffff00] decoration-{#ffff00} underline underline-offset-8 decoration-4"
+                : "mr-5 font-medium leading-6 text-white hover:text-[#ffff00]"
             }
           >
             INICIO
           </NavLink>
           <NavLink
             to="/services"
-            className={
-              "mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900"
+            className={({ isActive }) =>
+              isActive
+                ? "mr-5 font-medium leading-6 text-[#ffff00] hover:text-[#ffff00] decoration-{#ffff00} underline underline-offset-8 decoration-4"
+                : "mr-5 font-medium leading-6 text-white hover:text-[#ffff00]"
             }
           >
             SERVICIOS
           </NavLink>
           <NavLink
             to="/information"
-            className={
-              "mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900"
+            className={({ isActive }) =>
+              isActive
+                ? "mr-5 font-medium leading-6 text-[#ffff00] hover:text-[#ffff00] decoration-{#ffff00} underline underline-offset-8 decoration-4"
+                : "mr-5 font-medium leading-6 text-white hover:text-[#ffff00]"
             }
           >
             INFORMACION
@@ -44,7 +77,7 @@ const Navigation = () => (
 
           <a
             href="#_"
-            class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900"
+            class="mr-5 font-medium leading-6 text-white hover:text-yellow-200"
           >
             SWAP
           </a>
@@ -52,18 +85,8 @@ const Navigation = () => (
       </div>
 
       <div class="inline-flex items-center ml-5 space-x-6 lg:justify-end">
-        <a
-          href="#"
-          class="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
-        >
-          Sign in
-        </a>
-        <a
-          href="#"
-          class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
-        >
-          Sign up
-        </a>
+        <Network />
+        <Connect />
       </div>
     </div>
   </section>
